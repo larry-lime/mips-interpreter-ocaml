cd75f21e5417996940c6ca59a6d9c9c8114f08d7
Lawrence Lim <ll4715@nyu.edu>
2024-02-07 21:26:06 -0500

    everything works

--- mips_sim.ml
+++ mips_sim.ml
@@ -21,7 +21,6 @@ let decode_instruction (instruction : int32) =
   instformat2ins
     (match opcode with
     | 0l ->
-        (* TODO: Make sure this is right *)
         let r_rs = logand (shift_right instruction 21) 0x1Fl in
         let r_rt = logand (shift_right instruction 16) 0x1Fl in
         let r_rd = logand (shift_right instruction 11) 0x1Fl in
@@ -42,27 +41,27 @@ let step_add state r1 r2 r3 =
     Int32.add (rf_lookup (reg2ind r2) state.r) (rf_lookup (reg2ind r3) state.r)
   in
   let updated_rf = rf_update (reg2ind r1) sum state.r in
-  { state with r = updated_rf }
+  { state with r = updated_rf; pc = Int32.add state.pc 4l }
 
 let step_ori (state : state) (r1 : reg) (r2 : reg) (imm : int32) =
   let imm_or = Int32.logor (rf_lookup (reg2ind r2) state.r) imm in
   let updated_rf = rf_update (reg2ind r1) imm_or state.r in
-  { state with r = updated_rf }
+  { state with r = updated_rf; pc = Int32.add state.pc 4l }
 
 let step_lui (state : state) (r : reg) (imm : int32) =
   let imm_shifted = Int32.shift_left imm 16 in
   let updated_rf = rf_update (reg2ind r) imm_shifted state.r in
-  { state with r = updated_rf }
+  { state with r = updated_rf; pc = Int32.add state.pc 4l }
 
 let step_li (state : state) (r : reg) (imm : int32) =
   let updated_rf = rf_update (reg2ind r) imm state.r in
-  { state with r = updated_rf }
+  { state with r = updated_rf; pc = Int32.add state.pc 4l }
 
 let step_lw (state : state) (r1 : reg) (r2 : reg) (imm : int32) =
   let curPos = Int32.add (rf_lookup (reg2ind r2) state.r) imm in
   let word = read_word state.m curPos in
   let updated_rf = rf_update (reg2ind r1) word state.r in
-  { state with r = updated_rf }
+  { state with r = updated_rf; pc = Int32.add state.pc 4l }
 
 let step_sw (state : state) (r1 : reg) (r2 : reg) (imm : int32) =
   let curPos = Int32.add (rf_lookup (reg2ind r2) state.r) imm in
@@ -73,15 +72,26 @@ let step_sw (state : state) (r1 : reg) (r2 : reg) (imm : int32) =
          (mem_update (Int32.add curPos 2l) (getByte curWord 2)
             (mem_update (Int32.add curPos 3l) (getByte curWord 3) state.m)))
   in
-  { state with m = updated_mem }
+  { state with m = updated_mem; pc = Int32.add state.pc 4l }
 
 let step_beq (state : state) (r1 : reg) (r2 : reg) (imm : int32) =
   let r1_val = rf_lookup (reg2ind r1) state.r in
   let r2_val = rf_lookup (reg2ind r2) state.r in
   if Int32.equal r1_val r2_val then
     let updated_pc = Int32.add (Int32.add state.pc 8l) imm in
-    { state with pc = updated_pc }
-  else state
+    { state with pc = Int32.add updated_pc 4l }
+  else { state with pc = Int32.add state.pc 4l }
+
+let step_jr (state : state) (r : reg) =
+  let r_val = rf_lookup (reg2ind r) state.r in
+  { state with pc = r_val }
+
+let step_jal (state : state) (imm : int32) =
+  let return_address = Int32.add state.pc 8l in
+  let updated_rf = rf_update 31 return_address state.r in
+  let upper_bits = Int32.shift_left (Int32.shift_right state.pc 28) 28 in
+  let updated_pc = Int32.add upper_bits (Int32.shift_left imm 2) in
+  { state with pc = updated_pc; r = updated_rf }
 
 let rec interp (state : state) : state =
   let open Int32 in
@@ -93,16 +103,14 @@ let rec interp (state : state) : state =
       match instruction with
       | Add (r1, r2, r3) -> step_add state r1 r2 r3
       | Beq (r1, r2, imm) -> step_beq state r1 r2 imm
-      (* | Jr r -> step_jr init_state r *)
-      (* | Jal imm -> step_jal init_state imm *)
+      | Jr r -> step_jr state r
+      | Jal imm -> step_jal state imm
       | Li (r, imm) -> step_li state r imm
       | Lui (r, imm) -> step_lui state r imm
       | Ori (r1, r2, imm) -> step_ori state r1 r2 imm
       | Lw (r1, r2, imm) -> step_lw state r1 r2 imm
       | Sw (r1, r2, imm) -> step_sw state r1 r2 imm
-      | _ -> raise FatalError
     in
-    let updated_state = { updated_state with pc = add updated_state.pc 4l } in
     interp updated_state
 
 (*
